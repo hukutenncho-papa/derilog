f6aa5288832c665ddeb67ddce257690ca5d0d63d
hukutenncho-papa <no10fatbob0728@gmail.com>
2026-07-29 19:00:53 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -30,13 +30,7 @@ function formatMoney(value){
 
     const num = Number(value);
 
-    if(num >= 1000){
-
-        return (num / 1000).toFixed(1) + "K";
-
-    }
-
-    return num + "円";
+    return (num / 1000).toFixed(1) + "K";
 
 }
 
